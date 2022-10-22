58d7104f8e911d18ea3ed3b2511a2c2e4ef36088
Raul-Robotika <114672325+Raul-Robotika@users.noreply.github.com>
2022-10-22 12:13:06 +0200

    Update radionice-15-10-2022.h

--- radionice-15-10-2022.h
+++ radionice-15-10-2022.h
@@ -10,41 +10,83 @@ void RobotLine::radionica() {
   char simbol[10 + sizeof(char)];
 
   // ZADATAK: napravite if naredbu da se robot zaustavi nakon što se vrati na početak
-  if (broj >= 9){
-    stop(), end();
-    delayMs(500);
+  if (rightFront()<leftFront()){
+    if (rightFront() < 120) {
+      if (rightFront()>100)
+        go(80,20);
+       else
+        go(20,80);
+    }
   }
-  if (broj==2 and front() < 120){
-    go(-90, 90);
-    delayMs(500);
+  if (leftFront()<120) {
+    if (leftFront() >100)
+      go(20,80);
+     else
+      go(80,20);
   }
-  if (broj==2 and front() < 120){
-    go(-50, -50);
-    delayMs(500);
-    delayMs(500);
-    go(90,-90);
-    delayMs(500);
+  else{
+    go(50,50);
   }
-  if (front() < 120) {
+    if (broj==0 && front() < 120){
+    go(90, -90);
+    delayMs(500);
+    broj++;
+  }
+    if (broj==1 && front() < 120){
     go(-90, 90);
     delayMs(500);
     broj++;
   }
+    if (broj==2 && front() < 120){
+    go(-90, 90);
+    delayMs(500);
+    broj++;
+  }
+    if (broj==3 && front() < 120){
+    go(90, -90);
+    delayMs(500);
+    broj++;
+  }
+    if (broj==4 && front() < 120){
+    go(90, -90);
+    delayMs(500);
+    broj++;
+  }
+  if (broj==5 && front() < 120){
+    go(-90, -90);
+    delayMs(500);
+    go(90,-90);
+    delayMs(500);
+    broj++;
+  }
+    if (broj==6 && front() < 120){
+    go(90, -90);
+    delayMs(500);
+    broj++;
+  }
+    if (broj==7 && front() > 300 && leftFront() > 300){
+    go(-20,80);
+    delayMs(2000);
+    broj++;
+    }
+    stop();
+    end();
   if (rightFront() < leftFront()) {
     if (rightFront() > 120)
-      go(80, 20);
+      go(90, 30);
     else
-      go(20, 80);
+      go(30, 90);
   }
   else {
     if (leftFront() > 120) {
-      go(20, 80);
+      go(30, 90);
     }
     else {
-      go(80, 20);
+      go(90, 30);
     }
 
   }
   sprintf(simbol, "%d", broj);
   display(simbol);
 }
+
